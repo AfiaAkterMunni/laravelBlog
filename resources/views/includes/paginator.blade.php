88c8a6d2e33f501fc1fc6d626fb7659559736105
AfiaAkterMunni <afiamunni03@gmail.com>
2022-06-16 16:34:38 +0600

    paginator blade modify

--- resources/views/includes/paginator.blade.php
+++ resources/views/includes/paginator.blade.php
@@ -36,28 +36,3 @@
         </div>
     </div>
 @endif
-{{-- <div class="row">
-    <div class="column large-12">
-        <nav class="pgn">
-            <ul>
-                <li>
-                    <span class="pgn__prev" href="#0">
-                        Prev
-                    </span>
-                </li>
-                <li><a class="pgn__num" href="#0">1</a></li>
-                <li><span class="pgn__num current">2</span></li>
-                <li><a class="pgn__num" href="#0">3</a></li>
-                <li><a class="pgn__num" href="#0">4</a></li>
-                <li><a class="pgn__num" href="#0">5</a></li>
-                <li><span class="pgn__num dots">…</span></li>
-                <li><a class="pgn__num" href="#0">8</a></li>
-                <li>
-                    <span class="pgn__next" href="#0">
-                        Next
-                    </span>
-                </li>
-            </ul>
-        </nav>
-    </div>
-</div> --}}
